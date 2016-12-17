3dd3886eba904aa43d63ed290e57e55b7e1c7fcb
Pascal Steichen <pst@libre.lu>
2016-12-18 00:48:27 +0100

    adjusted getemails mgmt cmd

--- members/management/commands/getemails.py
+++ members/management/commands/getemails.py
@@ -11,7 +11,15 @@ class Command(BaseCommand):
   def handle(self, *args, **options):
     out=''
     for ty in args:
-      for m in Member.objects.filter(status=ty):
+      if ty == 'members':
+        query =  Member.objects.filter(status=Member.ACT) | Member.objects.filter(status=Member.HON) | Member.objects.filter(status=Member.WBE)
+      elif ty == 'board':
+        query = Member.objects.filter(role__isnull=False)
+      else:
+        query = None
+
+    if query is not None:
+      for m in query:
         out += '"' + m.first_name + ' ' + m.last_name + '" <' + m.email + '>, '
 
     self.stdout.write(re.sub('\, $', '', out))
