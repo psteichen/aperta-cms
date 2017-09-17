68c954c1c8c50b9143afcebe19855618346f8d21
Pascal Steichen <pst@libre.lu>
2017-09-17 23:17:10 +0200

    adjusted setup mgmt command

--- setup/management/commands/setup.py
+++ setup/management/commands/setup.py
@@ -13,41 +13,26 @@ class Command(BaseCommand):
   help = 'Adjusts settings file based on inital config via web interface'
 
   def handle(self, *args, **options):
-    # create secret key
-    generated_key = u''.join([SystemRandom().choice(ascii_letters + digits + punctuation) for _ in range(50)])
-#    settings.SECRET_KEY	= generated_key
-    self.stdout.write(self.style.SUCCESS(u"settings.SECRETY_KEY = "+generated_key))
-
     #adjust settings from setup app
     try:
       S = Setup.objects.latest('cfg_date')
       if S.admin_email: 
-#        settings.SERVER_EMAIL = S.admin_email
+        settings.SERVER_EMAIL = S.admin_email
         self.stdout.write(self.style.SUCCESS(u"settings.SERVER_EMAIL = "+S.admin_email))
-      if S.name: 
-#        settings.TEMPLATE_CONTENT['meta']['logo']['title'] = S.name
-        self.stdout.write(self.style.SUCCESS(u"settings.TEMPLATE_CONTENT['meta']['logo']['title'] = "+S.name))
-#        settings.TEMPLATE_CONTENT['meta']['title'] = S.name+" - Club Management System (CMS)"
-        self.stdout.write(self.style.SUCCESS(u"settings.TEMPLATE_CONTENT['meta']['title'] = "+S.name+u" - Club Management System (CMS)"))
-        if S.default_email: settings.DEFAULT_FROM_EMAIL = "'"+S.name+"' <"+S.default_email+">"
+      if S.org_name: 
+        settings.TEMPLATE_CONTENT['meta']['logo']['title'] = S.org_name
+        self.stdout.write(self.style.SUCCESS(u"settings.TEMPLATE_CONTENT['meta']['logo']['title'] = "+S.org_name))
+        settings.TEMPLATE_CONTENT['meta']['title'] = S.org_name+" - Club Management System (CMS)"
+        self.stdout.write(self.style.SUCCESS(u"settings.TEMPLATE_CONTENT['meta']['title'] = "+S.org_name+u" - Club Management System (CMS)"))
+        if S.default_email: settings.DEFAULT_FROM_EMAIL = "'"+S.org_name+"' <"+S.default_email+">"
       if S.default_footer: 
-#        settings.EMAILS['footer'] = S.default_footer
+        settings.EMAILS['footer'] = S.default_footer
         self.stdout.write(self.style.SUCCESS(u"settings.EMAILS['footer'] = "+S.default_footer))
-      if S.logo: 
-#        settings.TEMPLATE_CONTENT['meta']['logo']['img'] = S.logo
-        self.stdout.write(self.style.SUCCESS(u"settings.TEMPLATE_CONTENT['meta']['logo']['img'] = "+S.logo))
+      if S.org_logo: 
+        settings.TEMPLATE_CONTENT['meta']['logo']['img'] = S.org_logo
+        self.stdout.write(self.style.SUCCESS(u"settings.TEMPLATE_CONTENT['meta']['logo']['img'] = "+S.org_logo))
     except Setup.DoesNotExist:
       self.stdout.write(self.style.ERROR(u"No setup data found in database. First you need to configure the system via the webinterface."))
-
-    # finally set allowd host
-    site = Site.objects.get(pk=settings.SITE_ID)
-#    settings.ALLOWED_HOSTS[0] = site.name
-    self.stdout.write(self.style.SUCCESS(u"settings.ALLOWED_HOSTS[0] = "+site.name))
-
-# file method:
-#      with FileInput(fileToSearch, inplace=True, backup='.bak') as file:
-#      for line in file:
-#        line.replace(textToSearch, textToReplace)
 
     self.stdout.write(self.style.SUCCESS('Settings adjusted successfully.'))
 
